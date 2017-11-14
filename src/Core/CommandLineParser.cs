8fe31b747b7dfff7e8f2c1e961e091eb6a0882a0
Bryan Fawber <bryan.fawber@homenetauto.com>
2017-11-14 14:29:47 -0500

    comments pt 2

--- src/Core/CommandLineParser.cs
+++ src/Core/CommandLineParser.cs
@@ -7,17 +7,43 @@ using Core.Model;
 
 namespace Core
 {
+	/// <summary>
+	/// Parses the command line arguments
+	/// </summary>
 	public class CommandLineParser : ICommandLineParser
     {
+		/// <summary>
+		/// Holds a map of command line parameter name to the command line parameter class
+		/// </summary>
 		private readonly Dictionary<string, ICommandLineParameter> _commandLineParameters = new Dictionary<string, ICommandLineParameter>();
 
+		/// <summary>
+		/// A cache of the methods created while parsing the command line parameters
+		/// </summary>
 		private readonly Dictionary<Type, MethodInfo> _methodCache = new Dictionary<Type, MethodInfo>();
 
+		/// <summary>
+		/// Adds a new command line parameter to the parser's store
+		/// </summary>
+		/// <typeparam name="T">The type of the command line parameter</typeparam>
+		/// <param name="name">The name of the command line parameter</param>
+		/// <param name="prefix">The prefix expected before the command line parameter</param>
+		/// <param name="separator">The separator between the command line parameter key and its value</param>
+		/// <param name="isRequired">Is the command line parameter required</param>
+		/// <param name="hasValue">Does it have a value</param>
+		/// <param name="description">The description of the command line parameter</param>
 		public void AddParameter<T>(string name, string prefix = "-", string separator = "=", bool isRequired = true, bool hasValue = true, string description = "")
 		{
 			_commandLineParameters.Add(name, CommandLineParameterFactory.Create<T>(name, prefix, separator, isRequired, hasValue, description));
 		}
 
+		/// <summary>
+		/// Parses the command line arguments passed in to the object provided using the command line
+		/// parameter store.
+		/// </summary>
+		/// <typeparam name="T">The type of the object to parse into</typeparam>
+		/// <param name="args">The command line arguments</param>
+		/// <returns>The object with the properties populated with the command line argument values</returns>
 		public T Parse<T>(string[] args) where T : new()
 		{
 			T parametersContainer = new T();
@@ -31,7 +57,7 @@ namespace Core
 
 				if(!_methodCache.TryGetValue(property.PropertyType, out specificMethod))
 				{
-					MethodInfo genericMethod = typeof(CommandLineParser).GetRuntimeMethod(nameof(ConvertToType), new Type[] { typeof(string) });
+					MethodInfo genericMethod = typeof(CommandLineParser).GetRuntimeMethod(nameof(ConvertToType), new Type[] { typeof(object) });
 					specificMethod = genericMethod.MakeGenericMethod(property.PropertyType);
 					_methodCache.Add(property.PropertyType, specificMethod);
 				}
@@ -41,8 +67,12 @@ namespace Core
 
 			return parametersContainer;
 		}
-	   
-	    public string GetHelpString()
+
+		/// <summary>
+		/// Gets the string that shows the command line parameter names next to their descriptions
+		/// </summary>
+		/// <returns>A string that shows the command line parameter names next to their descriptions</returns>
+		public string GetHelpString()
 	    {
 			StringBuilder bob = new StringBuilder();
 		    foreach (var parameter in _commandLineParameters)
@@ -52,7 +82,13 @@ namespace Core
 		    return bob.ToString();
 	    }
 
-		public T ConvertToType<T>(string value)
+		/// <summary>
+		/// Converts the object to it's concrete type
+		/// </summary>
+		/// <typeparam name="T">The type to convert the object to</typeparam>
+		/// <param name="value">The value of the object</param>
+		/// <returns>The object in its concrete form</returns>
+		public T ConvertToType<T>(object value)
 		{
 			return (T)Convert.ChangeType(value, typeof(T));
 		}
--- src/Core/ICommandLineParser.cs
+++ src/Core/ICommandLineParser.cs
@@ -1,11 +1,34 @@
 ﻿namespace Core
 {
+	/// <summary>
+	/// An interface that holds methods needed for command line parsing
+	/// </summary>
     public interface ICommandLineParser
-    {
-	    void AddParameter<T>(string name, string prefix = "-", string separator = "=", bool isRequired = true, bool hasValue = true, string description = "");
+	{
+		/// <summary>
+		/// Adds a new command line parameter to the parser's store
+		/// </summary>
+		/// <typeparam name="T">The type of the command line parameter</typeparam>
+		/// <param name="name">The name of the command line parameter</param>
+		/// <param name="prefix">The prefix expected before the command line parameter</param>
+		/// <param name="separator">The separator between the command line parameter key and its value</param>
+		/// <param name="isRequired">Is the command line parameter required</param>
+		/// <param name="hasValue">Does it have a value</param>
+		/// <param name="description">The description of the command line parameter</param>
+		void AddParameter<T>(string name, string prefix = "-", string separator = "=", bool isRequired = true, bool hasValue = true, string description = "");
 
-	    T Parse<T>(string[] args) where T : new();
+		/// <summary>
+		/// Parses the command line arguments passed in to the object provided using the command line
+		/// parameter store.
+		/// </summary>
+		/// <typeparam name="T">The type of the object to parse into</typeparam>
+		/// <param name="args">The command line arguments</param>
+		/// <returns>The object with the properties populated with the command line argument values</returns>
+		T Parse<T>(string[] args) where T : new();
 
-	    string GetHelpString();
+		/// <summary>
+		/// Gets the string that shows the command line parameter names next to their descriptions
+		/// </summary>
+		string GetHelpString();
     }
 }
